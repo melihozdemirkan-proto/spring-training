28a87eff7ad6919ec5a283d783237e8deb4f55a1
Melih Ozdemirkan <melih.ozdemirkan@protoyazilim.com>
2021-03-09 21:02:06 +0300

    03-SpringBoot_Rest_IntegrationTest

--- src/test/java/com/proto/app/controller/MovieControllerTest.java
+++ src/test/java/com/proto/app/controller/MovieControllerTest.java
@@ -1,7 +1,27 @@
 package com.proto.app.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MovieControllerTest {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    public void getMoviesSuccess() throws Exception {
+
+        mvc.perform(get("/movies")) //
+                .andDo(print()) //
+                .andExpect(status().isOk());
+    }
 
 }
